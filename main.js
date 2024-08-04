5651ba3013f0f507d31e91d9243beeaa909de854
ywms12 <yazanw525@gmail.com>
2024-08-04 03:18:45 +0300

    practhtml

---
+++ main.js
@@ -0,0 +1,9 @@
+document.getElementById('showL').addEventListener('click', function(){
+    document.getElementById('u2').style.display = 'block';
+    document.getElementById('showL').style.display = 'none';
+})
+
+document.getElementById('hideL').addEventListener('click', function(){
+    document.getElementById('u2').style.display = 'none';
+    document.getElementById('showL').style.display = 'block';
+})
